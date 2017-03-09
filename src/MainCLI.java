900251ef4353b01b0054f1e63977290909b8aab9
bkesavab <kbpc4@hotmail.com>
2017-03-08 21:53:18 -0500

    Update MainCLI.java

--- src/MainCLI.java
+++ src/MainCLI.java
@@ -1,13 +1,16 @@
 public class MainCLI {
     public static void main(String[] args) {
+        //Here we create a new Planet Object
         Planet planet = new Planet(10);
         ReactiveAgent agent = new ReactiveAgent(planet);
 
-        int steps = 0;
+        int steps = 0;//This is a counter
+        
+        //This while loop runs as long as the steps varuable is under 100, and then quits the game
         while (steps < 100) {
-            agent.play();
-            steps += 1;
-            System.out.println(planet);
+            agent.play();//This command allows the player to play the game
+            steps += 1;//This is a counter for steps taken
+            System.out.println(planet);//Output return statement from Planet Class
         }
     }
 }
